74f045bf3971fb9d91ac29650831b80bd15f7718
Reil G <rgingoyon@circlepix.com>
2017-10-14 01:43:19 +0800

    made install.sh an executable

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+
+# Set GIT to use colors
+git config --global color.ui auto
+
+# Load up some default values from the environment!
+GIT_NAME_CURRENT=$(git config --global user.name)
+GIT_EMAIL_CURRENT=$(git config --global user.email)
+
+# Grab vimrc from repo
+
