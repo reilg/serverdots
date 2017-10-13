40a15a70744c908b802e21c845a71a41d35ab0dd
Reil G <rgingoyon@circlepix.com>
2017-10-14 01:57:37 +0800

    updated install script

--- install.sh
+++ install.sh
@@ -7,5 +7,52 @@ git config --global color.ui auto
 GIT_NAME_CURRENT=$(git config --global user.name)
 GIT_EMAIL_CURRENT=$(git config --global user.email)
 
-# Grab vimrc from repo
+# Ask what name to use?
+printf "Git Name [$GIT_NAME_CURRENT]:"
+read GIT_NAME_NEW
 
+# Ask what email to use?
+printf "Git Email [$GIT_EMAIL_CURRENT]:"
+read GIT_EMAIL_NEW
+
+# Save the GIT_NAME
+if [ "$GIT_NAME_NEW" != "" ]
+then
+	git config --global user.name "$GIT_NAME_NEW"
+fi
+
+# Save the GIT_EMAIL
+if [ "$GIT_EMAIL_NEW" != "" ]
+then
+	git config --global user.email "$GIT_EMAIL_NEW"
+fi
+
+# Where is the temp DIR for dotfiles?
+DOTFILES_TMP="${HOME}/.serverdots"
+
+# Check if we already have a copy
+if [ ! -d $DOTFILES_TMP ]
+then
+	# Now clone the repo...
+	git clone https://github.com/reilg/serverdots.git $DOTFILES_TMP
+else
+	# Or update existing shiz
+	cd $DOTFILES_TMP && git fetch && git checkout origin/master
+fi
+
+# Overwrite vimrc file
+cp -r $DOTFILES_TMP/.vimrc ~/.vimrc
+
+# Overwrite bashrc 
+cp -r $DOTFILES_TMP/.bashrc ~/.bashrc
+
+# Check if we want to install/update plugins
+if [ "$UPDATE_PLUGINS" != "n" ]
+then
+	# Install Vim Plugins
+	printf "Installing Vim Plugins, Will take a while depending on connection speed!\n"
+	nvim +PlugUpgrade +PlugClean! +PlugUpdate +qall
+fi
+
+# Install done! WEW!
+printf "Install DONE! WEW!\n"
